e794f2d2e82101e30e6d1b2b63ec10d7a07a2854
Aurimas Niekis <aurimas@niekis.lt>
2020-08-20 12:59:53 +0900

    Fixed Add Dev Deps script

--- add-dev-deps.sh
+++ add-dev-deps.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
- yarn add -D typescript ts-node nodemon gts eslint-plugin-prettier eslint-plugin-node eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin @types/node
+yarn add -D typescript ts-node nodemon gts eslint-plugin-prettier eslint-plugin-node eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin @types/node
+rm ./add-dev-deps.sh
