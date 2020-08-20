128c5ff79c4a4eea60f055ecb2ae86ccf9b4cb45
Aurimas Niekis <aurimas@niekis.lt>
2020-08-20 12:58:49 +0900

    Initial Commit

---
+++ add-dev-deps.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+ yarn add -D typescript ts-node nodemon gts eslint-plugin-prettier eslint-plugin-node eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin @types/node
